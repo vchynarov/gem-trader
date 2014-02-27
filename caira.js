9d61d53c714a4a263f6c33e1b0f386467d6ee950
Viktor Chynarov <viktor.chynarov@gmail.com>
2014-02-27 03:06:33 -0600

    game essentially done, must polish it up

--- caira.js
+++ caira.js
@@ -1,8 +1,15 @@
+/*
+ * 
+ * START DEFINITiONS
+ * */
 cities = new Object();
 gemMap = new Object();
+inventory = new Object();
+inventory["coins"] = 100; //initialize this!
 game = "#gameContainer";
 currentCity = "";
-money = 0;
+day = 0;
+
 
 function City(name, gems) {
     //gems is going to be passed as an array
@@ -19,14 +26,28 @@ function Gem(name, low, high) {
     gemMap[name] = this;
 }
 
-Gem.prototype.getPrice = function() { 
-    var range = this.high - this.low;
-    return Math.round(((Math.random()*1000) % range) + this.low);
-}
 
 Gem.prototype.newPrice = function() { 
     var range = this.high - this.low;
     this.price = Math.round(((Math.random()*1000) % range) + this.low);
+    return this.price;
+}
+
+Gem.prototype.buy = function() {
+    if(inventory["coins"] >= this.price) {
+        inventory["coins"] -= this.price;
+        inventory[this.name] += 1;
+        displayInventory();
+    }
+}
+
+Gem.prototype.sell = function() {
+    if(inventory[this.name] > 0) {
+        inventory["coins"] += this.price;
+        inventory[this.name] -= 1;
+        displayInventory();
+    }
+    
 }
 
 
@@ -40,7 +61,19 @@ diamond = new Gem("diamond", 100, 200);
 topaz = new Gem("topaz", 10, 25);
 emerald = new Gem("emerald", 20, 50);
 
+function initializeInventory() {
+    gemsList = Object.keys(gemMap);
+    for(var i=0; i<gemsList.length; i++) {
+        gem = gemsList[i]
+        inventory[gem] = 0;
+    }
+}
 
+/*
+ * 
+ * END OF DEFINITIONS
+ * 
+ * */
 function listPrint(parent, list) {
     //parent is the CSS selector of the parent
     var gemSelector = parent + "Gems"
@@ -51,8 +84,10 @@ function listPrint(parent, list) {
     $(parentid).append("<ul>");
     for(var i=0; i<list.length; i++) {
         gem = list[i]
-        listString="<li>" + "<span class='gemName'>" + gem + "</span>"+ "<span class='gemPrice'>" + gemMap[gem].getPrice() + "</span>" + "</li>";
-        $("#" + gemSelector).append(listString);
+        listingString="<li>" + "<span class='gemName'>" + gem + "</span>"+ "<span gemType='" + gem + "' class='sell " + gem + "'>Sell</span>" ;
+        tradeString="<span gemType='" + gem + "' class='buy " + gem + "'>Buy</span><span class='gemPrice " + gem + "'></span></li>";
+        finalString = listingString + tradeString;
+        $("#" + gemSelector).append(finalString);
     }
     $(parentid).append("</ul>");
 }
@@ -79,9 +114,10 @@ function display() {
             $("#" + city + "Gems").slideUp();
         }
     }
+    $("#days").text(day);
 }
 
-function setCityProperties() {
+function setProperties() {
     citiesList = Object.keys(cities)
     for(var i=0; i<citiesList.length; i++) {
         city = citiesList[i];
@@ -91,28 +127,58 @@ function setCityProperties() {
                 display();
             }
             else {
-                updatePrices(city);
                 currentCity = city;
+                updatePrices();
+                day += 1;
                 display();
             }
         });
     }
+    
+    $(".buy").click(function() {
+        gem = this["attributes"].getNamedItem("gemType").value;
+        gemMap[gem].buy();
+    });
+    
+    $(".sell").click(function() {
+        gem = this["attributes"].getNamedItem("gemType").value;
+        gemMap[gem].sell();
+    });
+
 }
 
-function updatePrices(city) {
-    for(var i=0; i<cities[city].gems; i++) {
-        gemMap[cities[city].gems[i]].newPrice();
+function updatePrices() {
+    gemsList = Object.keys(gemMap);
+    for(var i=0; i<gemsList.length; i++) {
+        gem = gemsList[i]
+        $(".gemPrice."+gem).text(gemMap[gem].newPrice());
     }
+}
+
+function displayInventory() {
+    itemsList = Object.keys(inventory);
+    for(var i=0; i<itemsList.length; i++) {
+        item = itemsList[i]
+        $("#"+item).text(inventory[item]);
+    }
+    
+    
 }
 function tick() {
     
     
 }
+
+
+    
 $(document).ready(function() { 
     divPrint(game, Object.keys(cities)); 
     
-    setCityProperties();
+    setProperties();
+    initializeInventory();
     display();
+    
+    displayInventory();
 
 });
 
