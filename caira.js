3477bd2451801e654445d234ba7a46666464b123
Viktor Chynarov <viktor.chynarov@gmail.com>
2014-02-27 01:40:20 -0600

    last good before a major facelift

--- caira.js
+++ caira.js
@@ -11,16 +11,11 @@ function City(name, gems) {
     cities[name] = this;
 }
 
-
-alexandria = new City("alexandria", ["ruby", "silver", "diamond"]);
-babylon = new City("babylon", ["ruby", "topaz", "emerald"]);
-cairo = new City("cairo", ["diamond", "topaz", "silver"]);
-
-
 function Gem(name, low, high) {
     this.name = name;
     this.low = low;
     this.high = high;
+    this.price = 0;
     gemMap[name] = this;
 }
 
@@ -29,6 +24,15 @@ Gem.prototype.getPrice = function() {
     return Math.round(((Math.random()*1000) % range) + this.low);
 }
 
+Gem.prototype.newPrice = function() { 
+    var range = this.high - this.low;
+    this.price = Math.round(((Math.random()*1000) % range) + this.low);
+}
+
+
+alexandria = new City("alexandria", ["ruby", "silver", "diamond"]);
+babylon = new City("babylon", ["ruby", "topaz", "emerald"]);
+cairo = new City("cairo", ["diamond", "topaz", "silver"]);
 
 ruby = new Gem("ruby", 30, 80);
 silver = new Gem("silver", 1, 10);
@@ -37,19 +41,20 @@ topaz = new Gem("topaz", 10, 25);
 emerald = new Gem("emerald", 20, 50);
 
 
-
-
 function listPrint(parent, list) {
     //parent is the CSS selector of the parent
     var gemSelector = parent + "Gems"
     var divString = "<div class='gemListing' id='" + gemSelector + "'></div>";
 
     parentid = "#" + parent;
-    $(parentid)[0].innerHTML+= divString;
+    $(parentid).append(divString);
+    $(parentid).append("<ul>");
     for(var i=0; i<list.length; i++) {
-        listString="<li>" + list[i] + "</li>"
-        $("#" + gemSelector)[0].innerHTML += listString;
+        gem = list[i]
+        listString="<li>" + "<span class='gemName'>" + gem + "</span>"+ "<span class='gemPrice'>" + gemMap[gem].getPrice() + "</span>" + "</li>";
+        $("#" + gemSelector).append(listString);
     }
+    $(parentid).append("</ul>");
 }
 
 function divPrint(parent, list) {
@@ -58,7 +63,7 @@ function divPrint(parent, list) {
         //this works, now each city has the proper id.
         city = list[i]
         listString='<div class="cityName" id="' + city + '">' + city +"</div>"
-        $(parent)[0].innerHTML += listString;
+        $(parent).append(listString);
         listPrint(city, cities[city].gems);
     }
 }
@@ -70,11 +75,9 @@ function display() {
         if(city == currentCity) {
             $("#" + city + "Gems").slideDown();
         }
-        
         else {
             $("#" + city + "Gems").slideUp();
         }
-        
     }
 }
 
@@ -82,29 +85,34 @@ function setCityProperties() {
     citiesList = Object.keys(cities)
     for(var i=0; i<citiesList.length; i++) {
         city = citiesList[i];
-        
         $("#" + city).click(function () {
             city = this.id;
             if(city==currentCity) {
                 display();
             }
             else {
+                updatePrices(city);
                 currentCity = city;
                 display();
             }
-            
         });
-
-        
     }
+}
+
+function updatePrices(city) {
+    for(var i=0; i<cities[city].gems; i++) {
+        gemMap[cities[city].gems[i]].newPrice();
+    }
+}
+function tick() {
     
     
 }
-
 $(document).ready(function() { 
     divPrint(game, Object.keys(cities)); 
+    
+    setCityProperties();
     display();
-    setCityProperties()
 
 });
 
